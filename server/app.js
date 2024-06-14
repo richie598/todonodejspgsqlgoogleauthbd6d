b95d87c7ea973476ffb7dc3157020fadb5b52345
Richard Watson <richie598@users.noreply.github.com>
2024-06-14 18:38:34 +0100

    Update app.js for user details

--- server/app.js
+++ server/app.js
@@ -38,8 +38,7 @@ const getRoles = (req, res) => {
   }
 
   // Uncomment this code below and plug in your own alias to test the contributor role
-  /*
-  if (body.userDetails === 'yourGoogleAlias@gmail.com') {
+  if (body.userDetails === 'info@eaffi.ie') {
     res.send({
       "roles": [
         "contributor"
@@ -48,7 +47,7 @@ const getRoles = (req, res) => {
 
     return;
   }
-  */
+  
 
   res.send({
     "roles": [
